5554671ab835ad3063fd244121e692f5eb0b4371
Andrew Morris <andrew.morris0202@gmail.com>
2024-10-07 19:23:40 -0400

    Added csv function for team

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once 'lib/plaintext.php';
 
@@ -36,7 +35,6 @@ $missionContent = readPlainText('data/mission.txt');
             </div>
         </div>
     </div>
-
        <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg navbar-light navbar-custom fixed-top" id="navbar">
         <div class="container">
@@ -295,214 +293,21 @@ $missionContent = readPlainText('data/mission.txt');
                 <div class="row justify-content-center mb-4">
                     <div class="col-lg-7 text-center">
                         <h2 class="fw-bold">Our Team Members</h2>
-                        <p class="text-muted">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem ab illo inventore.</p>
+                        <p class="text-muted">Meet our amazing team members!</p>
                     </div>
                     <!-- end col -->
                 </div>
                 <!-- end row -->
                 <div class="row">
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="team-box mt-4 position-relative overflow-hidden rounded text-center shadow">
-                            <div class="position-relative overflow-hidden">
-                                <img src="images/team/1.jpg" alt="" class="img-fluid d-block mx-auto" />
-                                <ul class="list-inline p-3 mb-0 team-social-item">
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-primary"><i class="icon-sm" data-feather="facebook"></i></a>
-                                    </li>
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-info"><i class="icon-sm" data-feather="twitter"></i></a>
-                                    </li>
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-danger"><i class="icon-sm" data-feather="instagram"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="p-4">
-                                <h5 class="font-size-19 mb-1">Frances Thompson</h5>
-                                <p class="text-muted text-uppercase font-size-14 mb-0">Developer</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end col -->
-
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="team-box mt-4 position-relative overflow-hidden rounded text-center shadow">
-                            <div class="position-relative overflow-hidden">
-                                <img src="images/team/2.jpg" alt="" class="img-fluid d-block mx-auto" />
-                                <ul class="list-inline p-3 mb-0 team-social-item">
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-primary"><i class="icon-sm" data-feather="facebook"></i></a>
-                                    </li>
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-info"><i class="icon-sm" data-feather="twitter"></i></a>
-                                    </li>
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-danger"><i class="icon-sm" data-feather="instagram"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="p-4">
-                                <h5 class="font-size-19 mb-1">John Jones</h5>
-                                <p class="text-muted text-uppercase font-size-14 mb-0">Ceo</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end col -->
-
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="team-box mt-4 position-relative overflow-hidden rounded text-center shadow">
-                            <div class="position-relative overflow-hidden">
-                                <img src="images/team/3.jpg" alt="" class="img-fluid d-block mx-auto" />
-                                <ul class="list-inline p-3 mb-0 team-social-item">
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-primary"><i class="icon-sm" data-feather="facebook"></i></a>
-                                    </li>
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-info"><i class="icon-sm" data-feather="twitter"></i></a>
-                                    </li>
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-danger"><i class="icon-sm" data-feather="instagram"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="p-4">
-                                <h5 class="font-size-19 mb-1">Della Hobbs</h5>
-                                <p class="text-muted text-uppercase font-size-14 mb-0">Designer</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end col -->
-
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="team-box mt-4 position-relative overflow-hidden rounded text-center shadow">
-                            <div class="position-relative overflow-hidden">
-                                <img src="images/team/4.jpg" alt="" class="img-fluid d-block mx-auto" />
-                                <ul class="list-inline p-3 mb-0 team-social-item">
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-primary"><i class="icon-sm" data-feather="facebook"></i></a>
-                                    </li>
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-info"><i class="icon-sm" data-feather="twitter"></i></a>
-                                    </li>
-                                    <li class="list-inline-item mx-3">
-                                        <a href="javascript: void(0);" class="team-social-icon h-danger"><i class="icon-sm" data-feather="instagram"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="p-4">
-                                <h5 class="font-size-19 mb-1">Troy Jordon</h5>
-                                <p class="text-muted text-uppercase font-size-14 mb-0">Developer</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end col -->
+                    <?php 
+                        read_team_data($csv_team);
+                    ?>
                 </div>
                 <!-- end row -->
             </div>
             <!-- end container -->
         </section>
         <!-- Team end -->
-
-        <!-- Blog start -->
-        <section class="section" id="blog">
-            <div class="container">
-                <div class="row justify-content-center mb-4">
-                    <div class="col-lg-7 text-center">
-                        <h2 class="fw-bold">Our Blog</h2>
-                        <p class="text-muted">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem ab illo inventore.</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="card mt-4 border-0 shadow">
-                            <div class="card-body p-4">
-                                <span class="badge badge-soft-primary">UI & UX Design</span>
-                                <h4 class="font-size-22 my-4"><a href="javascript: void(0);">Step bt step to conduct usability testing</a></h4>
-                                <p class="text-muted">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
-                                <div class="d-flex align-items-center mt-4 pt-2">
-                                    <img src="images/user/img-2.jpg" class="rounded-circle avatar-sm me-3" alt="..." />
-                                    <div class="flex-body">
-                                        <h5 class="font-size-17 mb-0">John Yeager</h5>
-                                        <p class="text-muted mb-0 font-size-14">Designer, New York</p>
-                                    </div>
-                                </div>
-                            </div><!-- end cardbody -->
-                        </div><!-- end card -->
-                    </div><!-- end col -->
-
-                    <div class="col-lg-4">
-                        <div class="card mt-4 border-0 shadow">
-                            <div class="card-body p-4">
-                                <span class="badge badge-soft-primary">CEO</span>
-                                <h4 class="font-size-22 my-4"><a href="javascript: void(0);">Increase conversion rate from ad to landing page</a></h4>
-                                <p class="text-muted">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
-                                <div class="d-flex align-items-center mt-4 pt-2">
-                                    <img src="images/user/img-3.jpg" class="rounded-circle avatar-sm me-3" alt="..." />
-                                    <div class="flex-body">
-                                        <h5 class="font-size-17 mb-0">Berneice Harris</h5>
-                                        <p class="text-muted mb-0 font-size-14">Designer, New York</p>
-                                    </div>
-                                </div>
-                            </div><!-- end cradbody -->
-                        </div><!-- end card -->
-                    </div>
-                    <!-- end col -->
-
-                    <div class="col-lg-4">
-                        <div class="card mt-4 border-0 shadow">
-                            <div class="card-body p-4">
-                                <span class="badge badge-soft-primary">Developer</span>
-                                <h4 class="font-size-22 my-4"><a href="javascript: void(0);">Why small business should start marketing</a></h4>
-                                <p class="text-muted">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
-                                <div class="d-flex align-items-center mt-4 pt-2">
-                                    <img src="images/user/img-1.jpg" class="rounded-circle avatar-sm me-3" alt="..." />
-                                    <div class="flex-body">
-                                        <h5 class="font-size-17 mb-0">Sarah Pettway</h5>
-                                        <p class="text-muted mb-0 font-size-14">Designer, New York</p>
-                                    </div>
-                                </div>
-                            </div><!-- end cardbody -->
-                        </div><!-- end card -->
-                    </div>
-                    <!-- end col -->
-                </div>
-                <!-- end row -->
-            </div>
-            <!-- end container -->
-        </section>
-        <!-- Blog end -->
-
-        <!-- CTA start -->
-        <section class="section bg-center w-100 bg-light" style="background-image: url(images/cta-bg.png);">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card bg-gradient-primary text-center border-0">
-                            <div class="bg-overlay-img" style="background-image: url(images/demos.png);"></div>
-                            <div class="card-body mx-auto p-sm-5 p-4">
-                                <div class="row justify-content-center">
-                                    <div class="col-lg-10">
-                                        <div class="p-3">
-                                            <h2 class="text-white mb-4">Join our Growing Community</h2>
-                                            <p class="text-white-70 font-size-16 mb-4 pb-3">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                                            <a href="javascript: void(0);" class="btn btn-light rounded-pill">Sign Up for free</a>
-                                        </div>
-                                    </div>
-                                    <!-- end col -->
-                                </div>
-                                <!-- end row -->
-                            </div>
-                            <!-- end cardbody -->
-                        </div>
-                        <!-- end card -->
-                    </div>
-                    <!-- end col -->
-                </div>
-                <!-- end row -->
-            </div>
-            <!-- end container -->
-        </section>
-        <!-- CTA end -->
         <!-- Contact Start -->
         <section class="section" id="contact">
             <div class="container">
--- lib/csv_function.php
+++ lib/csv_function.php
@@ -1,5 +1,5 @@
 <?php
-    $csv_file = 'lib/awards.csv';
+    $csv_file = 'data/awards.csv';
     function read_csv_file($file){
         $fp=fopen($file, 'r'); //open csv file
         fgetcsv($fp); //Skip headers
@@ -22,5 +22,41 @@
             };
         fclose($fp); //close file
     };
+
+    $csv_team = 'data/team.csv';
+    function read_team_data($file){
+        $fp=fopen($file, 'r');
+        $i = 1; //this will loop through each of the members pictures
+        fgetcsv($fp);
+        while($row = fgetcsv($fp)){
+            $member = $row[0];
+            $position = $row[1];
+            echo '
+            <div class="col-lg col-sm-6">
+                <div class="team-box mt-4 position-relative overflow-hidden rounded text-center shadow">
+                    <div class="position-relative overflow-hidden">
+                        <img src="images/team/' . $i . '.jpg" alt="" class="img-fluid d-block mx-auto" style="height: 325px"; />
+                        <ul class="list-inline p-3 mb-0 team-social-item">
+                            <li class="list-inline-item mx-3">
+                                <a href="javascript: void(0);" class="team-social-icon h-primary"><i class="icon-sm" data-feather="facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item mx-3">
+                                <a href="javascript: void(0);" class="team-social-icon h-info"><i class="icon-sm" data-feather="twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item mx-3">
+                                <a href="javascript: void(0);" class="team-social-icon h-danger"><i class="icon-sm" data-feather="instagram"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="p-4">
+                        <h5 class="font-size-19 mb-1">' . $member . '</h5>
+                        <p class="text-muted text-uppercase font-size-14 mb-0">' . $position . '</p>
+                    </div>
+                </div>
+            </div>';
+            $i++;
+        };
+    };
+
 ?>
 
